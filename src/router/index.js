408d22b0199e83e090b5afc663920a736b255779
Omar <90000500+Billioma@users.noreply.github.com>
2023-10-30 06:04:26 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: "/",
+    path: "/dashboard",
     name: "dashboard",
     component: () => import("../views/Dashboard.vue"),
   },
